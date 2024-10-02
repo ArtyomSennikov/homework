2bba92365f7ebcd03800e788ed31163eb85e35d7
ArtyomSennikov <artsen89@outlook.com>
2024-10-02 17:53:11 +0500

    Update variables.py

--- variables.py
+++ variables.py
@@ -2,4 +2,4 @@ homeworks_completed = 12
 hours_spent = 1.5
 course_name = 'Python'
 time_for_one_homework = (hours_spent / homeworks_completed)
-print('Course name: ' + course_name, 'Number of completed homeworks: ' + (str(homeworks_completed)), 'Hours spent: ' + (str(hours_spent)), 'Average execution time: ' + (str(time_for_one_homework)))
+print('Course name:', course_name, ' Number of completed homeworks:', homeworks_completed, ' Hours spent:', hours_spent, ' Average execution time:', time_for_one_homework)
