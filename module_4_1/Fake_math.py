f4751db72f79de148e4f54b0cd9a7dd1c3b2c2a7
ArtyomSennikov <artsen89@outlook.com>
2024-10-28 18:04:37 +0500

    Add files via upload

---
+++ module_4_1/Fake_math.py
@@ -0,0 +1,5 @@
+def divide(First, Second):
+    if Second == 0:
+        return 'Ошибка'
+    else:
+        return First / Second
---
+++ module_4_1/True_math.py
@@ -0,0 +1,7 @@
+from math import inf
+
+def divide(First, Second):
+    if Second == 0:
+        return inf
+    else:
+        return First / Second
