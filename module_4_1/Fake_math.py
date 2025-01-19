d4150d076025db9a9d016169e2f07b87f4b864db
ArtyomSennikov <artsen89@outlook.com>
2025-01-19 20:14:24 +0500

    Delete module_4_1 directory

--- module_4_1/Fake_math.py
+++
@@ -1,5 +0,0 @@
-def divide(First, Second):
-    if Second == 0:
-        return 'Ошибка'
-    else:
-        return First / Second
--- module_4_1/True_math.py
+++
@@ -1,7 +0,0 @@
-from math import inf
-
-def divide(First, Second):
-    if Second == 0:
-        return inf
-    else:
-        return First / Second
