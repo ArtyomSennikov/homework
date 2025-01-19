cd9204d31b4b02b1f5f460fc2d18120a400450d2
ArtyomSennikov <artsen89@outlook.com>
2025-01-19 17:25:47 +0500

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,5 +1,1 @@
-homeworks_completed = 12
-hours_spent = 1.5
-course_name = 'Python'
-time_for_one_homework = (hours_spent / homeworks_completed)
-print('Course name:', course_name, ' Number of completed homeworks:', homeworks_completed, ' Hours spent:', hours_spent, ' Average execution time:', time_for_one_homework)
+
